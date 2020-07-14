c90166feded7c806a5d5e12a1c7512bf270c5be3
LuisBlanche <loulouserr@gmail.com>
2020-07-14 15:23:47 +0000

    fix: pull_data and gitignore

--- pull_data.sh
+++ pull_data.sh
@@ -1,6 +1,7 @@
-kaggle download ttahara/birdsong-resampled-train-audio-00
-kaggle download ttahara/birdsong-resampled-train-audio-01
-kaggle download ttahara/birdsong-resampled-train-audio-02
-kaggle download ttahara/birdsong-resampled-train-audio-03
-kaggle download ttahara/birdsong-resampled-train-audio-04
+kaggle datasets download ttahara/birdsong-resampled-train-audio-00 -p data & 
+kaggle datasets download ttahara/birdsong-resampled-train-audio-01 -p data &
+kaggle datasets download ttahara/birdsong-resampled-train-audio-02 -p data &
+kaggle datasets download ttahara/birdsong-resampled-train-audio-03 -p data &
+kaggle competitions download -f train.csv birdsong-recognition -p data &
+unzip ./data/*.zip -p data/input & 
 
