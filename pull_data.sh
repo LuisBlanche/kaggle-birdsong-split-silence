562033e06e4ee2e49b631c3a52f45066ae21e52c
LuisBlanche <loulouserr@gmail.com>
2020-07-14 14:31:55 +0000

    datsasets

---
+++ pull_data.sh
@@ -0,0 +1,6 @@
+kaggle download ttahara/birdsong-resampled-train-audio-00
+kaggle download ttahara/birdsong-resampled-train-audio-01
+kaggle download ttahara/birdsong-resampled-train-audio-02
+kaggle download ttahara/birdsong-resampled-train-audio-03
+kaggle download ttahara/birdsong-resampled-train-audio-04
+
